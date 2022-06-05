c7d1d869112e59300ca1f5221300c60334832c95
Jacob Gorton <jagorton@iastate.edu>
2022-06-05 13:31:03 -0500

    added basic components

--- website/PersonalWebsite/src/app/app.module.ts
+++ website/PersonalWebsite/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
+import { HeaderComponent } from './header/header.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonalInfoComponent,
+    HeaderComponent,
+    SkillsComponent,
+    ExperienceComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
