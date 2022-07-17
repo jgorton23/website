e9dcac615dc62bb4a100eaacc85ec2bfde3dec5b
Jacob Gorton <jagorton@iastate.edu>
2022-07-17 11:21:55 -0500

    removed unused css and spec files

--- website/PersonalWebsite/src/app/skills/skills.component.ts
+++ website/PersonalWebsite/src/app/skills/skills.component.ts
@@ -15,21 +15,21 @@ export class SkillsComponent implements OnInit {
 
   skillsColOne: skill[] = [
     {skillName: "Java", years: 4, level: 5},
+    {skillName: "Git", years: 5, level: 5},
     {skillName: "Python", years: 1, level: 4},
     {skillName: "SQL", years: 2, level: 4},
     {skillName: "C/C++", years: 2, level: 4},
     {skillName: "Docker", years: 1, level: 3},
     {skillName: "AWS Lambda", years: 1, level: 2},
-    {skillName: "Git", years: 5, level: 5},
   ];
 
   skillsColTwo: skill[] = [
     {skillName: "JavaScript", years: 3, level: 4},
-    {skillName: "CSS", years: 5, level: 3},
-    {skillName: "HTML", years: 5, level: 3},
     {skillName: "Angular", years: 1, level: 4},
     {skillName: "React Native", years: 1, level: 3},
     {skillName: "React.js", years: 1, level: 3},
+    {skillName: "CSS", years: 5, level: 3},
+    {skillName: "HTML", years: 5, level: 3},
   ]
 
 }
