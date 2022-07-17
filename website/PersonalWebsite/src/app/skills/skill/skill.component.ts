2a0a74c22bac8c720747fb9813002e7200dcc285
Jacob Gorton <jagorton@iastate.edu>
2022-07-16 22:43:10 -0500

    added binding of skill level for stars

--- website/PersonalWebsite/src/app/skills/skill/skill.component.ts
+++ website/PersonalWebsite/src/app/skills/skill/skill.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-skill',
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillComponent implements OnInit {
 
+  @Input()
+  level: number | undefined;
+  
   constructor() { }
 
   showDetails: boolean = false;
